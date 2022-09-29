00abfb38aa0fede822d1b0676dbed6cc18907c48
AitanaCaro <aitana.caro@iesdoctorbalmis.com>
2022-09-29 12:35:26 +0200

    cambio nombre de variable textBlock

--- Tema2-FormatoTexto/MainWindow.xaml.cs
+++ Tema2-FormatoTexto/MainWindow.xaml.cs
@@ -24,35 +24,35 @@ namespace Tema2_FormatoTexto
         {
             InitializeComponent();
         }
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        private void userTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            modifiedtextTextBlock.Text = userTextBox.Text;
+            modifiedTextTextBlock.Text = userTextBox.Text;
         }
 
         private void coloresControl(object sender, RoutedEventArgs e)
         {
             if(blueRadioButton.IsChecked == true)
-                modifiedtextTextBlock.Foreground = Brushes.Blue; 
+                modifiedTextTextBlock.Foreground = Brushes.Blue; 
             else if(redRadioButton.IsChecked == true)
-                modifiedtextTextBlock.Foreground = Brushes.Red;
+                modifiedTextTextBlock.Foreground = Brushes.Red;
             else
-                modifiedtextTextBlock.Foreground = Brushes.Green;
+                modifiedTextTextBlock.Foreground = Brushes.Green;
         }
 
         private void boldCheckBox_Click(object sender, RoutedEventArgs e)
         {
             if (boldCheckBox.IsChecked == true)
-                modifiedtextTextBlock.FontWeight = FontWeights.Bold;
+                modifiedTextTextBlock.FontWeight = FontWeights.Bold;
             else
-                modifiedtextTextBlock.FontWeight = FontWeights.Normal;
+                modifiedTextTextBlock.FontWeight = FontWeights.Normal;
         }
 
         private void italicCheckBox_Click(object sender, RoutedEventArgs e)
         {
             if(italicCheckBox.IsChecked == true)
-                modifiedtextTextBlock.FontStyle = FontStyles.Italic;
+                modifiedTextTextBlock.FontStyle = FontStyles.Italic;
             else
-                modifiedtextTextBlock.FontStyle = FontStyles.Normal;
+                modifiedTextTextBlock.FontStyle = FontStyles.Normal;
         }
     }
 }
